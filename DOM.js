be8664424aa056bfea38c949279d32f5204c4eb4
Manoj4003 <manojknr16@gmail.com>
2025-10-11 13:15:40 +0530

    Fourth  commit

---
+++ DOM.js
@@ -0,0 +1,14 @@
+let  tagName=document.getElementsByTagName('h1');
+console.log(tagName);
+let className=document.getElementsByClassName("para-1");
+console.log(className);
+let idName=document.getElementById("#unique-para");
+console.log(idName);
+let userName=document.getElementsByName("name");
+console.log(userName)
+// let querySelector=document.querySelector(".para-1");
+// let querySelector=document.querySelector("#unique-para");
+
+// console.log(querySelector);
+let queryselectorAll=document.querySelectorAll("#unique-para");
+console.log(queryselectorAll);
